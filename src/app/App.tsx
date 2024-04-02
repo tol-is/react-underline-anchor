9fe1f5bbe77b891dcda4fef99f9a56d5a30e88c4
Tolis C <tolis.c@intercom.io>
2024-04-02 12:34:51 +0100

    single quote

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import { Code } from "../code"
-import { AnchorUnderline } from "../anchor-underline"
-import { Analytics } from "@vercel/analytics/react"
+import { AnchorUnderline } from '../anchor-underline'
+import { Analytics } from '@vercel/analytics/react'
 
-import styles from "./styles.module.css"
+import styles from './styles.module.css'
 
-const nav = ["React", "Typescript", "HTML", "CSS", "Animation", "Underline", "Navigation"]
+const nav = ['React', 'Typescript', 'HTML', 'CSS', 'Animation', 'Underline', 'Navigation']
 
 const code = `<AnchorUnderline speed={0.5} />`
 
@@ -23,38 +22,38 @@ export const App = () => {
           </AnchorUnderline>
         </p>
         <p>
-          Renders staggered entry and exit underline transitions and implements an{" "}
+          Renders staggered entry and exit underline transitions and implements an{' '}
           <AnchorUnderline href="#">abstract speed</AnchorUnderline> prop, that configures the relative velocity of the transition. Using
-          CSS custom properies you can further customize the underline{" "}
-          <AnchorUnderline href="#" style={{ "--underline-color": "#00ffcc", "--underline-size": "2px" } as React.CSSProperties}>
+          CSS custom properies you can further customize the underline{' '}
+          <AnchorUnderline href="#" style={{ '--underline-color': '#00ffcc', '--underline-size': '2px' } as React.CSSProperties}>
             color
           </AnchorUnderline>
-          ,{" "}
-          <AnchorUnderline href="#" style={{ "--underline-size": "100%" } as React.CSSProperties}>
+          ,{' '}
+          <AnchorUnderline href="#" style={{ '--underline-size': '100%' } as React.CSSProperties}>
             size
-          </AnchorUnderline>{" "}
-          ,{" "}
-          <AnchorUnderline href="#" style={{ "--underline-position-y": "60%" } as React.CSSProperties}>
+          </AnchorUnderline>
+          ,{' '}
+          <AnchorUnderline href="#" style={{ '--underline-position-y': '60%' } as React.CSSProperties}>
             position
           </AnchorUnderline>
-          ,{" "}
+          ,{' '}
           <AnchorUnderline
             href="#"
             speed={false}
             style={
               {
-                "--underline-duration": "3s",
+                '--underline-duration': '3s',
               } as React.CSSProperties
             }>
             duration
           </AnchorUnderline>
-          , and{" "}
+          , and{' '}
           <AnchorUnderline
             href="#"
             style={
               {
-                "--underline-ease-in": "cubic-bezier(0.16, 1, 0.3, 1)",
-                "--underline-ease-out": "cubic-bezier(0.87, 0, 0.13, 1)",
+                '--underline-ease-in': 'cubic-bezier(0.16, 1, 0.3, 1)',
+                '--underline-ease-out': 'cubic-bezier(0.87, 0, 0.13, 1)',
               } as React.CSSProperties
             }>
             transition curve
@@ -63,10 +62,10 @@ export const App = () => {
         </p>
 
         <p>
-          Internally the component, is{" "}
+          Internally the component, is{' '}
           <AnchorUnderline href="#">
             measuring the length of its text contents, and dynamically computes the animation duration,
-          </AnchorUnderline>{" "}
+          </AnchorUnderline>{' '}
           so the transition feels <AnchorUnderline href="#">consistent across applications</AnchorUnderline>, within paragraphs, or
           standalone navigation links.
         </p>
